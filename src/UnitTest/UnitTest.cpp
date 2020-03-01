61ddcb1b070fe8fdb343eccc6dd87e9bd6ac55e4
vladimir Novick <vlad.novick@gmail.com>
2020-03-01 22:26:09 +0200

    Modify UtitTest

--- src/UnitTest/UnitTest.cpp
+++ src/UnitTest/UnitTest.cpp
@@ -7,14 +7,14 @@ using namespace RPN;
 using namespace std;
 namespace UnitTest
 {
-	TEST_CLASS(Compiler)
+	TEST_CLASS(GetTokens)
 	{
 	public:
 
-		void log(char *message, string dat) {
+		void log(char* message, string dat) {
 			Logger::WriteMessage((string(message) + dat.c_str() + "\n").c_str());
 		}
-		
+
 		TEST_METHOD(PrepareTokens)
 		{
 			string sourceString = "((15 / (7 - (A + 1))) * 3) - (B + (1 + 1))";
@@ -39,6 +39,17 @@ namespace UnitTest
 			Assert::AreEqual("( a * b + c * d ) / ( a - d ) + b * c ", ret.c_str());
 
 		};
+
+	};
+
+	TEST_CLASS(SimpleOperation)
+	{
+	public:
+
+		void log(char* message, string dat) {
+			Logger::WriteMessage((string(message) + dat.c_str() + "\n").c_str());
+		}
+
 
 		TEST_METHOD(Compile)
 		{
@@ -104,6 +115,19 @@ namespace UnitTest
 			log("Prepare Tokens > target: ", tokens);
 			log("RPN String > target: ", ret);
 			Assert::AreEqual("a b * c + d * e f / * ", ret.c_str());
+		};
+
+		TEST_METHOD(Compile6)
+		{
+			string sourceString = "a+b*(c-d/(10+x)+y)-e";
+			RPNProcessor processor(sourceString);
+			auto tokens = processor.PrepareTokens();
+			auto ret = processor.Prepare();
+
+			log("Prepare Tokens > source: ", sourceString);
+			log("Prepare Tokens > target: ", tokens);
+			log("RPN String > target: ", ret);
+			//Assert::AreEqual("a b * c + d * e f / * ", ret.c_str());
 		};
 	};
 }
